f3499f5178cd0e73d00d0ba79b2937c3860f27c3
KakiEugenio <137253448+KakiEugenio@users.noreply.github.com>
2023-06-21 05:58:48 +0100

    Create Lanche.java

---
+++ SnackBar1/src/snackbar1/Lanche.java
@@ -0,0 +1,32 @@
+
+package snackbar1;
+
+/**
+ *
+ * @author Pedro Malaquias
+ */
+public class Lanche extends Prato {
+    
+      private String tipo_pao;
+    private String molho;
+
+    public String getTipo_pao() {
+        return tipo_pao;
+    }
+
+    public void setTipo_pao(String tipo_pao) {
+        this.tipo_pao = tipo_pao;
+    }
+
+    public String getMolho() {
+        return molho;
+    }
+
+    public void setMolho(String molho) {
+        this.molho = molho;
+    }
+    
+    
+    
+    
+}
