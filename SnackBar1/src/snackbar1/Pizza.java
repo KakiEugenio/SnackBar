1a6cfc088da6a94f5a95f5956464e8cd6566dacf
KakiEugenio <137253448+KakiEugenio@users.noreply.github.com>
2023-06-21 05:58:52 +0100

    Create Pizza.java

---
+++ SnackBar1/src/snackbar1/Pizza.java
@@ -0,0 +1,30 @@
+
+package snackbar1;
+
+/**
+ *
+ * @author Pedro Malaquias
+ */
+public class Pizza extends Prato {
+    
+      private String cobertura;
+    private String molho;
+
+    public String getCobertura() {
+        return cobertura;
+    }
+
+    public void setCobertura(String cobertura) {
+        this.cobertura = cobertura;
+    }
+
+    public String getMolho() {
+        return molho;
+    }
+
+    public void setMolho(String molho) {
+        this.molho = molho;
+    }
+    
+    
+}
